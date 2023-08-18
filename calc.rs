9129b6211c1a0e5d9f27b24f91e0788e0b0c65a1
Jyothish157 <128952185+Jyothish157@users.noreply.github.com>
2023-08-18 18:23:03 +0530

    Update calc.rs

--- calc.rs
+++ calc.rs
@@ -1,1 +1,46 @@
+use std::io;
 
+fn main() {
+    println!("Simple Calculator");
+    println!("1. Addition");
+    println!("2. Subtraction");
+    println!("3. Multiplication");
+    println!("4. Division");
+
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice).expect("Failed to read line");
+    let choice: u32 = choice.trim().parse().expect("Please enter a valid number");
+    println!("Enter two numbers:");
+
+    let mut num1 = String::new();
+    io::stdin().read_line(&mut num1).expect("Failed to read line");
+    let num1: f64 = num1.trim().parse().expect("Please enter a valid number");
+
+    let mut num2 = String::new();
+    io::stdin().read_line(&mut num2).expect("Failed to read line");
+    let num2: f64 = num2.trim().parse().expect("Please enter a valid number");
+
+    match choice {
+        1 => println!("Result: {}", add(&num1, &num2)),
+        2 => println!("Result: {}", subtract(&num1, &num2)),
+        3 => println!("Result: {}", multiply(&num1, &num2)),
+        4 => println!("Result: {}", divide(&num1, &num2)),
+        _ => println!("Invalid choice"),
+    }
+}
+
+fn add(a: &f64, b: &f64) -> f64 {
+    a + b
+}
+
+fn subtract(a: &f64, b: &f64) -> f64 {
+    a - b
+}
+
+fn multiply(a: &f64, b: &f64) -> f64 {
+    a * b
+}
+
+fn divide(a: &f64, b: &f64) -> f64 {
+    a / b
+}
